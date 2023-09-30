9aee97ba3656bb1deb38b9d5021bf8cd6927de8e
ToEzBit <jirayut52328172@hotmail.com>
2023-09-30 21:14:39 +0700

    :sparkles: auth

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function RegisterPage() {
-  return <div>Register</div>;
-}
-
-export default RegisterPage;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div className="">This is a protected route.</div>;
+  return (
+    <div className="">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
